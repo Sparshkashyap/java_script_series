21a88177ff123d71bc7e91e078732a29abaaa02f
sparshkashyap <sparshkashyap655@gmail.com>
2025-01-04 16:26:29 +0530

    Basic Information about java_script

---
+++ practice_01/pr.js
@@ -0,0 +1,127 @@
+/*
+there are 7 datatype which is exist in primitive datatype
+symbol , null , undefined , String , Number, BigInt , Boolean
+
+there are mainly 3 datatype which is exist in java script 
+array , function , object
+
+
+*/
+
+
+
+// let num =100;
+// let st =String(num);
+// console.log(typeof num);
+// console.log(typeof st);
+
+// let num= new Number(100);
+// console.log(typeof num);
+
+// console.log(Number("abc"/10));
+
+// let sp="1";
+// let bo=Boolean(sp);
+// console.log(bo);
+
+// console.log(2+"1"+3);
+// console.log(null>=0);
+
+
+
+// let list=(1,2,3);
+// let max=Math.max(list);
+// console.log(max);
+
+// let num=156446548416487457465484954254912445n;
+// console.log(typeof num);
+
+// let num =Symbol(1255555);
+// console.log(typeof num);
+
+// let num=new Number(100);
+
+// let variable=null;
+// console.log(typeof variable);
+
+// let num=undefined;
+// console.log( typeof num);
+
+// let num=100;
+// console.log(num++);
+// console.log(num);
+// console.log(++num);
+
+// let num=new Number(123);
+// // console.log(num);
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(2))
+
+// let array=["spagdad",12,12.2255,true];
+
+// console.log(array.indexOf(true));
+
+// let p=array.join("");
+// console.log(p);
+
+// let num=new Number(1154646864);
+// console.log(num.toLocaleString('en-IN'));
+
+// find the random number between two numbers
+
+// let num1=10;
+// let num2=20;
+
+// // console.log(Math.floor(Math.random()*100+1));
+// console.log(Math.floor(Math.random()*(num2-num1+1)+num1));
+
+// let name="sparsh";
+// let age=20;
+// console.log(`my name is ${name} and my umar is ${age}`);
+
+// let name="Sparshkashyap";
+// console.log(name.substring(-10,5));
+
+
+// let mydate = new Date;
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.getMonth()+1);
+// console.log(mydate.getTime());
+// console.log(mydate.getHours());
+// console.log(mydate.getFullYear());
+// console.log(mydate.toLocaleTimeString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ two_Basics/arrays.js
@@ -0,0 +1,9 @@
+const arr=[1,2,3,4];
+
+const arr1=["sparsh",1,true,12.526,5];
+
+console.log(arr1[0]); //sparsh
+
+console.log(arr[6]); //3
+
+
