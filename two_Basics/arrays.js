601edd981c3b13780fbbde5609ee66f8917bdd86
sparshkashyap <sparshkashyap655@gmail.com>
2025-01-04 16:58:54 +0530

    Array ka gyan

--- two_Basics/arrays.js
+++ two_Basics/arrays.js
@@ -1,9 +1,59 @@
-const arr=[1,2,3,4];
+// const arr=[1,2,3,4];
 
-const arr1=["sparsh",1,true,12.526,5];
+// const arr1=["sparsh",1,true,12.526,5];
 
-console.log(arr1[0]); //sparsh
+// console.log(arr1[0]); 
 
-console.log(arr[6]); //3
+// console.log(typeof arr[0]);
+
+
+//  array methods
+
+// const arr=[1,2,3,4];
+//   Insert the Element at the last position 
+// arr.push(5,5,5);
+// console.log(arr);
+
+// Remove the Elememnt at the last position
+// console.log(arr.pop());
+// console.log(arr);
+
+// Insert the Element at the first position 
+
+// const myarr1=arr.unshift(15,25);
+// console.log(myarr1);
+// console.log(arr);
+
+// remove the Element at the last position
+
+// const myarr2=arr.shift()
+// console.log(myarr2);
+// console.log(arr);
+
+
+ // slice , splice 
+
+// =================Slice=====================================================
+const arr1=[1,2,3,4,5];
+let myslice=arr1.slice(0,3);
+
+console.log("A","->",myslice);
+console.log(arr1.length);
+
+
+// ====================Splice======================================================
+const arr2=[1,2,3,4,5];
+let myslic=arr2.splice(0,3);
+
+console.log("B","->",myslic);
+console.log(arr2.length);
+
+
+
+
+
+
+
+
 
 
